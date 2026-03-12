4427f1a078c72b0a1dc6d9dd3df5d836a4184b5c
Zefir Kirilov <contact@zefir.pro>
2026-03-12 19:59:11 +0200

    use private static field instead of scoped const

--- src/components/ServiceRow.ts
+++ src/components/ServiceRow.ts
@@ -4,49 +4,49 @@ import { Component } from "./Component";
 import { Service } from "../models/Service";
 import { ServiceStatus } from "../models/ServiceStatus";
 
-const STATUS_STYLES: Record<
-  ServiceStatus,
-  { color: string; label: string; icon: string }
-> = {
-  [ServiceStatus.OPERATIONAL]: {
-    color: "text-emerald-400",
-    label: "Operational",
-    icon:
-      `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm45.66,85.66-56,56a8,8,0,0,1-11.32,0l-24-24a8,8,0,0,1,11.32-11.32L112,148.69l50.34-50.35a8,8,0,0,1,11.32,11.32Z"></path>`,
-  },
-  [ServiceStatus.UNDER_MAINTENANCE]: {
-    color: "text-indigo-400",
-    label: "Under Maintenance",
-    icon:
-      `<path d="M128 24a104 104 0 1 0 104 104A104.13 104.13 0 0 0 128 24m14.052 54.734a34.2 34.2 0 0 1 9.427 1.006 3.79 3.79 0 0 1 1.865 6.25l-17.76 19.265 2.682 12.485 12.484 2.677 19.266-17.782a3.79 3.79 0 0 1 6.25 1.865 34.4 34.4 0 0 1 1.02 8.333 34.122 34.122 0 0 1-47.833 31.282l-24.672 28.536a4 4 0 0 1-.187.203 15.168 15.168 0 0 1-21.448-21.453q.098-.095.203-.182l28.542-24.667a34.155 34.155 0 0 1 30.161-47.818" />`,
-  },
-  [ServiceStatus.DEGRADED_PERFORMANCE]: {
-    color: "text-yellow-400",
-    label: "Degraded Performance",
-    icon:
-      `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm-8,56a8,8,0,0,1,16,0v56a8,8,0,0,1-16,0Zm8,104a12,12,0,1,1,12-12A12,12,0,0,1,128,184Z"></path>`,
-  },
-  [ServiceStatus.PARTIAL_OUTAGE]: {
-    color: "text-orange-400",
-    label: "Partial Outage",
-    icon:
-      `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm-8,56a8,8,0,0,1,16,0v56a8,8,0,0,1-16,0Zm8,104a12,12,0,1,1,12-12A12,12,0,0,1,128,184Z"></path>`,
-  },
-  [ServiceStatus.MAJOR_OUTAGE]: {
-    color: "text-red-400",
-    label: "Major Outage",
-    icon:
-      `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm37.66,130.34a8,8,0,0,1-11.32,11.32L128,139.31l-26.34,26.35a8,8,0,0,1-11.32-11.32L116.69,128,90.34,101.66a8,8,0,0,1,11.32-11.32L128,116.69l26.34-26.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>`,
-  },
-};
-
 @customElement("service-row")
 export class ServiceRow extends Component {
+  private static readonly STATUS_STYLES: Record<
+    ServiceStatus,
+    { color: string; label: string; icon: string }
+  > = {
+    [ServiceStatus.OPERATIONAL]: {
+      color: "text-emerald-400",
+      label: "Operational",
+      icon:
+        `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm45.66,85.66-56,56a8,8,0,0,1-11.32,0l-24-24a8,8,0,0,1,11.32-11.32L112,148.69l50.34-50.35a8,8,0,0,1,11.32,11.32Z"></path>`,
+    },
+    [ServiceStatus.UNDER_MAINTENANCE]: {
+      color: "text-indigo-400",
+      label: "Under Maintenance",
+      icon:
+        `<path d="M128 24a104 104 0 1 0 104 104A104.13 104.13 0 0 0 128 24m14.052 54.734a34.2 34.2 0 0 1 9.427 1.006 3.79 3.79 0 0 1 1.865 6.25l-17.76 19.265 2.682 12.485 12.484 2.677 19.266-17.782a3.79 3.79 0 0 1 6.25 1.865 34.4 34.4 0 0 1 1.02 8.333 34.122 34.122 0 0 1-47.833 31.282l-24.672 28.536a4 4 0 0 1-.187.203 15.168 15.168 0 0 1-21.448-21.453q.098-.095.203-.182l28.542-24.667a34.155 34.155 0 0 1 30.161-47.818" />`,
+    },
+    [ServiceStatus.DEGRADED_PERFORMANCE]: {
+      color: "text-yellow-400",
+      label: "Degraded Performance",
+      icon:
+        `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm-8,56a8,8,0,0,1,16,0v56a8,8,0,0,1-16,0Zm8,104a12,12,0,1,1,12-12A12,12,0,0,1,128,184Z"></path>`,
+    },
+    [ServiceStatus.PARTIAL_OUTAGE]: {
+      color: "text-orange-400",
+      label: "Partial Outage",
+      icon:
+        `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm-8,56a8,8,0,0,1,16,0v56a8,8,0,0,1-16,0Zm8,104a12,12,0,1,1,12-12A12,12,0,0,1,128,184Z"></path>`,
+    },
+    [ServiceStatus.MAJOR_OUTAGE]: {
+      color: "text-red-400",
+      label: "Major Outage",
+      icon:
+        `<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm37.66,130.34a8,8,0,0,1-11.32,11.32L128,139.31l-26.34,26.35a8,8,0,0,1-11.32-11.32L116.69,128,90.34,101.66a8,8,0,0,1,11.32-11.32L128,116.69l26.34-26.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>`,
+    },
+  };
+
   @property({ type: Object })
   public service!: Service;
 
   protected renderIcon(): TemplateResult {
-    const style = STATUS_STYLES[this.service.status];
+    const style = ServiceRow.STATUS_STYLES[this.service.status];
     return html`
       <span>
         <svg
