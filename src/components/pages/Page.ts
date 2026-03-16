3c4f0c5858279d24e8630e46d023d9b0b85d64ce
Zefir Kirilov <contact@zefir.pro>
2026-03-16 09:00:48 +0200

    improved routing accessibility

--- src/components/pages/Page.ts
+++ src/components/pages/Page.ts
@@ -1,4 +1,15 @@
 import { Component } from "../Component";
 
 export abstract class Page extends Component {
+  public override connectedCallback() {
+    super.connectedCallback();
+    this.tabIndex = -1;
+    this.classList.add("focus:outline-none");
+  }
+
+  public focus() {
+    this.updateComplete.then(() => {
+      super.focus();
+    });
+  }
 }
